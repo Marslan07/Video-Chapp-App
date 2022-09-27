6690fedf96b45910fc1a20c52b46c727b5416a2f
Arslan Ghani <m.arslan6845@gmail.com>
2022-09-27 11:25:47 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const cors= require('cors');
+const path = require("path");
+const PORT=process.env.PORT || 5000;
+
+const io = require('socket.io')(server, { cors: { origin: 'http://localhost:3000' } });
+
+// module.exports = {
+// 	//...
+// 	devServer: {
+// 		hot:true,
+// 		// host: '0.0.0.0'		
+// 	  },
+// 	  target:'web',
+// 	  mode:'development'
+//   };
+
+io.on("connection", (socket) => {
+	// debugger
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		debugger
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		debugger
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+if(process.env.NODE_ENV== "production"){
+	app.use(express.static("frontend/build"));
+	const path=require(path);
+	app.get("*",(req,res)=>{
+		res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+	})
+}
+
+server.listen(PORT,() => console.log(`server is running on port ${PORT}`))
